5387653df55febf4e37f710844706f8c538a4899
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2023-11-29 08:34:23 -0800

    Added Html CSS and Js code, Added logic of generating random number and expression, evaluating the expression and consoling the expression

---
+++ app.js
@@ -0,0 +1,34 @@
+function randomNum() {
+    let num = Math.floor(Math.random()*5)+1;
+    return num;
+}
+
+function randomExp() {
+    let exp = ['*','/','+','-'];
+    return exp[Math.floor(Math.random()*exp.length)];
+}
+
+
+let num1 = randomNum();
+let num2 = randomNum();
+let num3 = randomNum();
+let num4 = randomNum();
+let num5 = randomNum();
+
+let exp1 = randomExp();
+let exp2 = randomExp();
+let exp3 = randomExp();
+let exp4 = randomExp();
+
+console.log(`${num1} ${exp1} ${num2} ${exp2} ${num3} ${exp3} ${num4} ${exp4} ${num5}`);
+
+let expression = `${num1} ${exp1} ${num2} ${exp2} ${num3} ${exp3} ${num4} ${exp4} ${num5}`;
+let result;
+
+try {
+    result = eval(expression);
+    result = Math.round(result);
+    console.log("Result:", result);
+} catch (error) {
+    console.log("Invalid expression:", error);
+}
